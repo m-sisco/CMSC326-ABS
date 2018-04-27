73dcf92aa0668d45b7206cd5a7ef20b7def17c29
m-sisco <marissa.sisco@richmond.edu>
2018-04-27 11:20:12 -0400

    maybe added mutation?

--- Disease.java
+++ Disease.java
@@ -11,13 +11,44 @@ public class Disease {
         int l = r.nextInt(10)+1;
         for (int i = 0; i < l; i++){
             int s = r.nextInt(2);
-            disease = disease + s;
+            char c=(char)(s+'0');
+            disease = disease + c;
+        }
+    }
+
+    public Disease(Disease d){
+        Random r = new Random();
+        int mut = r.nextInt(10);
+        String dis = d.getDisease();
+        int len = dis.length();
+        String newDis;
+        if(mut == 0){
+            int bit = r.nextInt(len);
+            for(int i = 0; i < len; i++){
+                if(i == bit){
+                    char let = dis.charAt(i);
+                    if(let == '0'){
+                        newDis += '1';
+
+                    }else{
+                        newDis += '0';
+                    }
+                }
+                char let = dis.charAt(i);
+                newDis += let;
+            }
         }
     }
 
     public String getDisease(){
         return disease;
     }
+
+    public static void main(String [] args){
+        Disease d;
+        System.out.print(d.getDisease());
+    }
+
 
 
 }
